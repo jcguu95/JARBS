ed371689c9d800dd9b445364616e5b93e670aafb
jcguu95 <jcguu95@gmail.com>
2020-10-30 21:04:41 -0500

    removed things i don't want

--- main.sh
+++ main.sh
@@ -14,10 +14,10 @@ while getopts ":a:r:b:p:h" o; do case "${o}" in
 	*) printf "Invalid option: -%s\\n" "$OPTARG" && exit ;;
 esac done
 
-[ -z "$progsfile" ] && progsfile="https://raw.githubusercontent.com/jcguu95/JARBS/master/aux/progs.csv"
 [ -z "$aurhelper" ] && aurhelper="yay"
 [ -z "$repobranch" ] && repobranch="main"
 [ -z "$dotfilesrepo" ] && dotfilesrepo="https://github.com/jcguu95/tilde.git"
+[ -z "$progsfile" ] && progsfile="https://raw.githubusercontent.com/jcguu95/JARBS/master/aux/progs.csv"
 
 ### FUNCTIONS ###
 
@@ -34,24 +34,24 @@ getuserandpass() { \
 		read name || exit
 	done
 	echo "Enter a password for that user."
-	read pass1 || exit
+	read -s pass1 || exit
 	echo "Retype password."
-	read pass2 || exit
+	read -s pass2 || exit
 	while ! [ "$pass1" = "$pass2" ]; do
 		unset pass2
-		echo "Passwords do not match.\\n\\nEnter password again."
-		read pass1 || exit
+		echo "Passwords do not match. Enter password again."
+		read -s pass1 || exit
 		echo "Retype password."
-		read pass2 || exit
+		read -s pass2 || exit
 	done ;}
 
 usercheck() { \
 	! (id -u "$name" >/dev/null) 2>&1 ||
-	echo "The user \`$name\` already exists on this system. LARBS can install for a user already existing, but it will \\Zboverwrite\\Zn any conflicting settings/dotfiles on the user account.\\n\\nLARBS will \\Zbnot\\Zn overwrite your user files, documents, videos, etc., so don't worry about that, but only click <CONTINUE> if you don't mind your settings being overwritten.\\n\\nNote also that LARBS will change $name's password to the one you just gave."
+	echo "The user \`$name\` already exists on this system. JARBS can install for a user already existing."
 	}
 
 preinstallmsg() { \
-	echo "The rest of the installation will now be totally automated, so you can sit back and relax.\n\nIt will take some time, but when done, you can relax even more with your complete system.\n\nNow just press <Let's go!> and the system will begin installation!" || { clear; exit; }
+	echo "The rest of the installation will now be totally automated." || { clear; exit; }
 	}
 
 adduserandpass() { \
@@ -69,8 +69,8 @@ refreshkeys() { \
 	}
 
 newperms() { # Set special sudoers settings for install (or after).
-	sed -i "/#LARBS/d" /etc/sudoers
-	echo "$* #LARBS" >> /etc/sudoers ;}
+	sed -i "/#JARBS/d" /etc/sudoers
+	echo "$* #JARBS" >> /etc/sudoers ;}
 
 manualinstall() { # Installs $1 manually if not installed. Used only for AUR helper here.
 	[ -f "/usr/bin/$1" ] || (
@@ -91,7 +91,7 @@ maininstall() { # Installs all needed programs from main repo.
 gitmakeinstall() {
 	progname="$(basename "$1" .git)"
 	dir="$repodir/$progname"
-	echo "Installing \`$progname\` ($n of $total) via \`git\` and \`make\`. $(basename "$1") $2" 
+	echo "Installing \`$progname\` ($n of $total) via \`git\` and \`make\`. $(basename "$1") $2"
 	sudo -u "$name" git clone --depth 1 "$1" "$dir" >/dev/null 2>&1 || { cd "$dir" || return ; sudo -u "$name" git pull --force origin master;}
 	cd "$dir" || exit
 	make >/dev/null 2>&1
@@ -99,13 +99,13 @@ gitmakeinstall() {
 	cd /tmp || return ;}
 
 aurinstall() { \
-	echo "Installing \`$1\` ($n of $total) from the AUR. $1 $2" 
+	echo "Installing \`$1\` ($n of $total) from the AUR. $1 $2"
 	echo "$aurinstalled" | grep -q "^$1$" && return
 	sudo -u "$name" $aurhelper -S --noconfirm "$1" >/dev/null 2>&1
 	}
 
 pipinstall() { \
-	echo "Installing the Python package \`$1\` ($n of $total). $1 $2" 
+	echo "Installing the Python package \`$1\` ($n of $total). $1 $2"
 	command -v pip || installpkg python-pip >/dev/null 2>&1
 	yes | pip install "$1"
 	}
@@ -127,7 +127,7 @@ installationloop() { \
 	done < /tmp/progs.csv ;}
 
 putgitrepo() { # Downloads a gitrepo $1 and places the files in $2 only overwriting conflicts
-	echo "Downloading and installing config files..." 
+	echo "Downloading and installing config files..."
 	[ -z "$3" ] && branch="master" || branch="$repobranch"
 	dir=$(mktemp -d)
 	[ ! -d "$2" ] && mkdir -p "$2"
@@ -136,14 +136,15 @@ putgitrepo() { # Downloads a gitrepo $1 and places the files in $2 only overwrit
 	sudo -u "$name" cp -rfT "$dir" "$2"
 	}
 
-systembeepoff() { echo "Getting rid of that retarded error beep sound..." 
+systembeepoff() { echo "Getting rid of that retarded error beep sound..."
 	rmmod pcspkr
 	echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf ;}
 
 finalize(){ \
-	echo "Preparing welcome message..." 
-	echo "Congrats! Provided there were no hidden errors, the script completed successfully and all the programs and configuration files should be in place.\\n\\nTo run the new graphical environment, log out and log back in as your new user, then run the command \"startx\" to start the graphical environment (it will start automatically in tty1).\\n\\n.t Luke" 
+	echo "Done!"
 	}
+
+
 
 ### THE ACTUAL SCRIPT ###
 
@@ -168,13 +169,9 @@ refreshkeys || error "Error automatically refreshing Arch keyring. Consider doin
 
 # Get packages for installing and configuring other programs.
 for x in curl base-devel git ntp zsh; do
-	echo "Installing \`x\` which is required to install and configure other programs." 
+	echo "Installing \`x\` which is required to install and configure other programs."
 	installpkg "$x"
 done
-
-# Sync time.
-echo "Synchronizing system time to ensure successful and secure installation of software..." 
-ntpdate 0.us.pool.ntp.org >/dev/null 2>&1
 
 # Add a new user.
 adduserandpass || error "Error adding username and/or password."
@@ -185,10 +182,6 @@ adduserandpass || error "Error adding username and/or password."
 # Allow user to run sudo without password. Since AUR programs must be installed
 # in a fakeroot environment, this is required for all builds with AUR.
 newperms "%wheel ALL=(ALL) NOPASSWD: ALL"
-
-# Make pacman and yay colorful and adds eye candy on the progress bar because why not.
-grep -q "^Color" /etc/pacman.conf || sed -i "s/^#Color$/Color/" /etc/pacman.conf
-grep -q "ILoveCandy" /etc/pacman.conf || sed -i "/#VerbosePkgLists/a ILoveCandy" /etc/pacman.conf
 
 # Use all cores for compilation.
 sed -i "s/-j2/-j$(nproc)/;s/^#MAKEFLAGS/MAKEFLAGS/" /etc/makepkg.conf
@@ -202,7 +195,7 @@ manualinstall $aurhelper || error "Failed to install AUR helper."
 installationloop
 # Most packages are installed at this point. Below are some patches to the system.
 
-echo "Finally, installing \`libxft-bgra\` to enable color emoji in suckless software without crashes." 
+echo "Finally, installing \`libxft-bgra\` to enable color emoji in suckless software without crashes."
 yes | sudo -u "$name" $aurhelper -S libxft-bgra-git >/dev/null 2>&1
 
 ## # Install the dotfiles in the plug directory
@@ -224,25 +217,25 @@ chsh -s /bin/zsh "$name" >/dev/null 2>&1
 sudo -u "$name" mkdir -p "/home/$name/.cache/zsh/"
 
 # Tap to click
-[ ! -f /etc/X11/xorg.conf.d/40-libinput.conf ] && printf 'Section "InputClass"
-        Identifier "libinput touchpad catchall"
-        MatchIsTouchpad "on"
-        MatchDevicePath "/dev/input/event*"
-        Driver "libinput"
-	# Enable left mouse button by tapping
-	Option "Tapping" "on"
-EndSection' > /etc/X11/xorg.conf.d/40-libinput.conf
+### [ ! -f /etc/X11/xorg.conf.d/40-libinput.conf ] && printf 'Section "InputClass"
+###         Identifier "libinput touchpad catchall"
+###         MatchIsTouchpad "on"
+###         MatchDevicePath "/dev/input/event*"
+###         Driver "libinput"
+### 	# Enable left mouse button by tapping
+### 	Option "Tapping" "on"
+### EndSection' > /etc/X11/xorg.conf.d/40-libinput.conf
 
 # Fix fluidsynth/pulseaudio issue.
-grep -q "OTHER_OPTS='-a pulseaudio -m alsa_seq -r 48000'" /etc/conf.d/fluidsynth ||
-	echo "OTHER_OPTS='-a pulseaudio -m alsa_seq -r 48000'" >> /etc/conf.d/fluidsynth
+### grep -q "OTHER_OPTS='-a pulseaudio -m alsa_seq -r 48000'" /etc/conf.d/fluidsynth ||
+### 	echo "OTHER_OPTS='-a pulseaudio -m alsa_seq -r 48000'" >> /etc/conf.d/fluidsynth
 
 # Start/restart PulseAudio.
-killall pulseaudio; sudo -u "$name" pulseaudio --start
+### killall pulseaudio; sudo -u "$name" pulseaudio --start
 
 # This line, overwriting the `newperms` command above will allow the user to run
 # serveral important commands, `shutdown`, `reboot`, updating, etc. without a password.
-newperms "%wheel ALL=(ALL) ALL #LARBS
+newperms "%wheel ALL=(ALL) ALL #JARBS
 %wheel ALL=(ALL) NOPASSWD: /usr/bin/shutdown,/usr/bin/reboot,/usr/bin/systemctl suspend,/usr/bin/wifi-menu,/usr/bin/mount,/usr/bin/umount,/usr/bin/pacman -Syu,/usr/bin/pacman -Syyu,/usr/bin/packer -Syu,/usr/bin/packer -Syyu,/usr/bin/systemctl restart NetworkManager,/usr/bin/rc-service NetworkManager restart,/usr/bin/pacman -Syyu --noconfirm,/usr/bin/loadkeys,/usr/bin/yay,/usr/bin/pacman -Syyuw --noconfirm"
 
 # Last message! Install complete!
